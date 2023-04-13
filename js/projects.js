43b99468f62c55868793752e3c979069caba86c7
Beshoy Wageeh <85949107+beshoywageeh@users.noreply.github.com>
2023-04-13 21:02:50 +0200

    new protofolio design

---
+++ js/projects.js
@@ -0,0 +1,28 @@
+let data = [
+  {
+    name: "mini pos app",
+    description:
+      "Mini Pos Application for small business support multi language with sales invoice build with laravel.",
+    github: "https://github.com/beshoywageeh/pos",
+    image:
+      "https://raw.githubusercontent.com/beshoywageeh/pos/main/public/assets/img/screenshots/2.png",
+    live: "#",
+  },
+  {
+    name: "Old Portofolio Design",
+    description: "My Previous Portofolio Design Made With Sass And Javascript.",
+    github: "https://github.com/beshoywageeh/old-portofolio",
+    image:
+      "https://github.com/beshoywageeh/old-portofolio/raw/main/img/SharedScreenshot.jpg?raw=true",
+    live: "https://beshoywageeh.github.io/old-portofolio/",
+  },
+  {
+    name: "human resources managment",
+    description:
+      "Human Resources Managment Managment Web Based App Bulid With Laravel.",
+    github: "https://github.com/beshoywageeh/hrm",
+    image:
+      "https://d85wutc1n854v.cloudfront.net/live/products/icons/WB02N8N90.jpg?v=1.0",
+    live: "#",
+  },
+];
--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
 feather.replace();
+let projects = document.querySelector("#projects");
+
+data.map(append_data);
+function append_data(item) {
+  projects.innerHTML += `<div id="${
+    item.name
+  }" class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 mr-4">
+<a href="${item.github}">
+   <img class="rounded-t-lg" src="${item.image}" alt="" />
+</a>
+<div class="p-5">
+   <a href="${item.github}">
+       <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${
+         item.name
+       }</h5>
+   </a>
+   <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${
+     item.description
+   }"</p>
+   <a href="${item.github}" target="_blank" class="inline-flex 
+    items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+       Check The Repo<i class="fab fa-github fa-2x ml-2 -mr-1"></i>
+   </a>
+   ${
+     item.live != "#"
+       ? `  <a
+         href="${item.live}"
+         target="_blank"
+         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+       >
+         Check Live<i class="fas fa-link fa-2x ml-2 -mr-1"></i>
+       </a>
+     `
+       : ""
+   }
+</div>
+</div>`;
+}
